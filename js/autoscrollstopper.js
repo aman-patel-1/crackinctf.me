10e51356b3487df3a12c47f244096fe1d18aad79
aman__patel <87331432+aman-patel-1@users.noreply.github.com>
2024-11-15 09:51:45 +0000

    Your commit message here

---
+++ js/autoscrollstopper.js
@@ -0,0 +1,5 @@
+// Prevent auto-scroll on load
+                                window.history.scrollRestoration = 'manual';
+                                window.addEventListener('load', function() {
+                                  window.scrollTo(0, 0); // Forces page to start at the top
+                                });
