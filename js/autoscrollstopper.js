f3690b02937944e981ae61b58f31cec19a6037bb
aman__patel <87331432+aman-patel-1@users.noreply.github.com>
2024-11-15 14:57:06 +0000

    timline nav and t@c update

--- js/autoscrollstopper.js
+++ js/autoscrollstopper.js
@@ -1,5 +1,4 @@
 // Prevent auto-scroll on load
-                                window.history.scrollRestoration = 'manual';
-                                window.addEventListener('load', function() {
-                                  window.scrollTo(0, 0); // Forces page to start at the top
-                                });
+window.history.scrollRestoration = 'manual';
+window.addEventListener('load', function() {
+window.scrollTo(0, 0);});
